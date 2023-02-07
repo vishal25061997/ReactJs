b055dee43dd67e110bc5f4de04ab35fd7819b1da
vishal25061997 <121218252+vishal25061997@users.noreply.github.com>
2023-02-07 15:17:24 +0530

    Adding Expense items normally

---
+++ App.js
@@ -0,0 +1,14 @@
+
+
+import ExpenseItems from './components/ExpenseItems';
+
+function App() {
+  return (
+    <div >
+      
+      <ExpenseItems></ExpenseItems>
+    </div>
+  );
+}
+
+export default App;
---
+++ ExpenseItems.js
@@ -0,0 +1,10 @@
+function ExpenseItems(){
+    return <> <h2>Expense Items!</h2>
+    <h3>food Rs 10</h3>
+    <h3>petrol Rs 100</h3>
+    <h3>movies Rs 200</h3>
+    </>
+}
+
+
+export default ExpenseItems
