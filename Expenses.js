103c8d63ebb016918c3aa51017424136907fc3c8
vishal25061997 <121218252+vishal25061997@users.noreply.github.com>
2023-04-03 19:02:53 +0530

    rendering-expense-optimisation

--- Expenses.js
+++ Expenses.js
@@ -4,6 +4,7 @@ import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesChart from './ExpensesChart';
+import ExpenseItem from './ExpenseItem';
 
 
 const Expenses = (props) => {
@@ -25,8 +26,14 @@ const Expenses = (props) => {
              selected={filteredYear} 
              onChangeFilter={filterChangeHandler} 
              />
-             <ExpensesChart expenses = {filteredExpenses}/>
-             <ExpensesList items = {filteredExpenses}/>
+             
+             {props.item.map((expense)=>(
+             <ExpenseItem
+             key={expense.id}
+             title={expense.title}
+             amount={expense.amount}
+             date={expense.date}/>
+             ))}
           </Card>
           </div>
     );
